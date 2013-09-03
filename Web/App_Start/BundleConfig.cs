20a5e6ea1cacf456376816e7f9d9a575a3c8c220
Kiril Knysh <Kiril_Knysh@epam.com>
2013-09-03 16:08:05 +0300

    Initial project skeleton; FB auth;

---
+++ Web/App_Start/BundleConfig.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Web {
+	public class BundleConfig {
+		// For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.UseCdn = true;
+			
+			bundles.Add(new ScriptBundle("~/bundles/jquery", "//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js").Include(
+				"~/js/lib/jquery-{version}.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/auth").Include(
+				"~/js/auth/externalAuth.js"));
+
+			//bundles.Add(new ScriptBundle("~/bundles/todo").Include(
+			//	"~/Scripts/app/todo.bindings.js",
+			//	"~/Scripts/app/todo.datacontext.js",
+			//	"~/Scripts/app/todo.model.js",
+			//	"~/Scripts/app/todo.viewmodel.js"));
+
+		}
+	}
+}
---
+++ Web/js/auth/externalAuth.js
@@ -0,0 +1,71 @@
+﻿(function ($, window) {
+	var auth = {},
+		infoContainer = $("#logInInfo");
+		loginButtons = $('button[data-exteranl-type]');
+
+	loginButtons.on('click', function loginClick(e) {
+		var type = $(e.target).data('exteranl-type');
+		if (auth[type]) {
+			infoContainer.length && infoContainer.html("Authorizing...");
+			auth[type]();
+		}
+	});
+
+	function onLoginSuccess(id, name) {
+		var infoStr = "id: " + id + "  name: " + name;
+
+		infoContainer.length && infoContainer.html(infoStr);
+	};
+
+	auth.fbLogin = function () {
+		$.ajaxSetup({ cache: true });
+		$.getScript('//connect.facebook.net/en_US/all/debug.js', function () {
+			FB.init({
+				appId: '659022654109831'
+			});
+
+			FB.login(function fbLogged(response) {
+				if (response.authResponse) {
+					FB.api('/me', function fbGetMeInfo(meResponse) {
+						onLoginSuccess(response.authResponse.userID, meResponse.name);
+					});
+				}
+			});
+		});
+	};
+	auth.twLogin = function () {
+		//$.ajax({
+		//	type: "POST",
+		//	url: 'https://api.twitter.com/oauth/request_token'
+		//}).done(function () {
+		//	debugger;
+		//});
+
+
+		//$.when($.getScript('js/auth/twitter/sha1.js'), $.getScript('js/auth/twitter/codebird.js')).done(function twFilesLoaded() {
+		//	var cb = new Codebird();
+		//	cb.setConsumerKey("ZZ4twtX1E7lLd2I1UKjOdA", "CryOUCyEy0PZODVoPF3UEBolVTru708SbPFvDlf03s");
+		//	cb.__call(
+		//		"oauth_requestToken",
+		//		{ oauth_callback: "oob" },
+		//		function (reply) {
+    	//			// stores it
+    	//			cb.setToken(reply.oauth_token, reply.oauth_token_secret);
+
+    	//			// gets the authorize screen URL
+    	//			cb.__call(
+		//				"oauth_authorize",
+		//				{},
+		//				function (auth_url) {
+		//					window.codebird_auth = window.open(auth_url);
+		//				}
+		//			);
+		//		}
+		//	);
+		//});
+	};
+	auth.vkLogin = function () { };
+
+	window.auth = window.auth || {};
+	window.auth.external = auth;
+}(jQuery, window));
